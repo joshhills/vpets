676af65ad0595370aa5145e8e92708a6b4b1d28b
Josh Hills <josh.hills@jagex.com>
2020-01-31 16:00:57 +0000

    fix(master): tweaking test context

--- VPetsIntegrationTests/ControllerIntegrationTests.cs
+++ VPetsIntegrationTests/ControllerIntegrationTests.cs
@@ -9,15 +9,14 @@ using VPets;
 using VPets.Domain.Models;
 using VPets.Resources;
 using Xunit;
-using static VPets.Domain.Models.Metric;
 
 namespace VPetsIntegrationTests
 {
-    public class PetControllerIntegrationTests : IClassFixture<CustomWebApplicationFactory<Startup>> 
+    public class ControllerIntegrationTests : IClassFixture<CustomWebApplicationFactory<Startup>> 
     {
         private readonly HttpClient client;
 
-        public PetControllerIntegrationTests(CustomWebApplicationFactory<Startup> factory)
+        public ControllerIntegrationTests(CustomWebApplicationFactory<Startup> factory)
         {
             client = factory.CreateClient();
         }
@@ -25,6 +24,7 @@ namespace VPetsIntegrationTests
         [Fact]
         public async Task CanGetPets()
         {
+
             // Arrange
             var request = "/api/v1/pets";
 
@@ -185,6 +185,147 @@ namespace VPetsIntegrationTests
 
             // Assert
             Assert.Equal(HttpStatusCode.BadRequest, httpPostResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CanGetUsers()
+        {
+            // Arrange
+            var request = "/api/v1/users";
+
+            // Act
+            var httpResponse = await client.GetAsync(request);
+
+            // Assert response
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Assert content
+            var jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<IEnumerable<UserResource>>(jsonResponse);
+            Assert.True(response.ToList().Count > 1);
+        }
+
+        [Fact]
+        public async Task CanGetUser()
+        {
+            // Arrange
+            var request = "/api/v1/users/1";
+
+            // Act
+            var httpResponse = await client.GetAsync(request);
+
+            // Assert response
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Assert content
+            var jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<UserResource>(jsonResponse);
+            Assert.Equal("Josh", response.Name);
+        }
+
+        [Fact]
+        public async Task CanGetPetsForUser()
+        {
+            // Arrange
+            var request = "/api/v1/users/1/pets";
+
+            // Act
+            var httpResponse = await client.GetAsync(request);
+
+            // Assert response
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Assert content
+            var jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<IEnumerable<UserPetResource>>(jsonResponse);
+            Assert.True(response.ToList().Count > 1);
+        }
+
+        [Fact]
+        public async Task CannotGetUnknownUser()
+        {
+            // Arrange
+            var request = "/api/v1/users/999";
+
+            // Act
+            var httpResponse = await client.GetAsync(request);
+
+            // Assert response
+            var response = await httpResponse.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.NotFound, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CannotCreateUserWithBadRequest()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/api/v1/users",
+                Body = new
+                {
+                    Name = "-!@'/a:X.-!@'/a:X.-!@'/a:X.-!@'/a:X."
+                }
+            };
+
+            // Act
+            var httpResponse = await client.PostAsync(request.Url,
+                new StringContent(JsonConvert.SerializeObject(request.Body), Encoding.Default, "application/json"));
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CanCreateUser()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/api/v1/users",
+                Body = new
+                {
+                    Name = "Johnny"
+                }
+            };
+
+            // Act
+            var httpResponse = await client.PostAsync(request.Url,
+                new StringContent(JsonConvert.SerializeObject(request.Body), Encoding.Default, "application/json"));
+            var jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<UserResource>(jsonResponse);
+            Assert.Equal("Johnny", response.Name);
+        }
+
+        [Fact]
+        public async Task CanDeleteUser()
+        {
+            // Arrange
+            var createRequest = new
+            {
+                Url = "/api/v1/users",
+                Body = new
+                {
+                    Name = "Delete Me"
+                }
+            };
+
+            var httpCreateResponse = await client.PostAsync(createRequest.Url,
+                new StringContent(JsonConvert.SerializeObject(createRequest.Body), Encoding.Default, "application/json"));
+
+            var jsonCreateResponse = await httpCreateResponse.Content.ReadAsStringAsync();
+            var createResponse = JsonConvert.DeserializeObject<UserResource>(jsonCreateResponse);
+
+            var deleteRequest = $"/api/v1/users/{createResponse.Id}";
+
+            // Act
+            var httpResponse = await client.DeleteAsync(deleteRequest);
+
+            // Assert response
+            httpResponse.EnsureSuccessStatusCode();
+            var jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<UserResource>(jsonResponse);
+            Assert.Equal("Delete Me", response.Name);
         }
     }
 }
